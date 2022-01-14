7e5ea768a37bfd1a2fc82bd197007d593412e06a
Htet Myat Aung <57471977+aungmyathtet123@users.noreply.github.com>
2022-01-14 11:12:09 +0630

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//google login
+Route::get('login/google', 'Auth\LoginController@redirectToGoogle')->name('login.google');
+Route::get('login/google/callback', 'Auth\LoginController@handleGoogleCallback');
+//faceboklogin
+Route::get('login/facebook', 'Auth\LoginController@redirectToFacebook')->name('login.facebook');
+Route::get('login/facebook/callback', 'Auth\LoginController@handleFacebookCallback');
+//github login
+Route::get('login/github', 'Auth\LoginController@redirectToGithub')->name('login.github');
+Route::get('login/github/callback', 'Auth\LoginController@handleGithubCallback');
